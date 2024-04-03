49f1c58f31fd9ee7f1fbeee0b828f734dc8fed67
wanwisaMungchareonklang <S6419410021@sau.ac.th>
2024-04-03 13:09:14 +0700

    Code V.2: mukrata

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,11 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:mukrata_sau_app/views/about_ui.dart';
+import 'package:mukrata_sau_app/views/calculate_pay_bill_ui.dart';
+import 'package:mukrata_sau_app/views/menu_shop_ui.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 
 class HomeUI extends StatefulWidget {
   const HomeUI({super.key});
@@ -8,8 +15,60 @@ class HomeUI extends StatefulWidget {
 }
 
 class _HomeUIState extends State<HomeUI> {
+  List<Widget> showUI = [
+    CalculatePayBillUI(),
+    MenuShopUI(),
+    AboutUI(),
+  ];
+
+  int _currentIndex=1;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.deepOrange,
+        title: Text(
+          'Tech SAU BUFFET',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: ConvexAppBar(
+        onTap: (value) {
+          setState(() {
+            _currentIndex = value;
+          });
+        },
+        initialActiveIndex: _currentIndex,
+        backgroundColor: Colors.deepOrange,
+        items: [
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.moneyBill1Wave,
+              color: Color.fromARGB(255, 255, 221, 0),
+            ),
+            title: 'คิดเงิน',
+          ),
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.houseFlag,
+              color: Color.fromARGB(255, 235, 200, 5),
+            ),
+            title: 'Home',
+          ),
+          TabItem(
+            icon: Icon(
+              FontAwesomeIcons.tableList,
+              color: Color.fromARGB(255, 235, 200, 5),
+            ),
+            title: 'เกี่ยวกับ',
+          ),
+        ],
+      ),
+      body: showUI[_currentIndex],
+    );
   }
 }
--- lib/views/menu_shop_ui.dart
+++ lib/views/menu_shop_ui.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, prefer_final_fields
+
 import 'package:flutter/material.dart';
 
 class MenuShopUI extends StatefulWidget {
@@ -8,8 +10,106 @@ class MenuShopUI extends StatefulWidget {
 }
 
 class _MenuShopUIState extends State<MenuShopUI> {
+  List<String> _showMenu = [
+    'assets/images/img1.png',
+    'assets/images/img2.png',
+    'assets/images/img3.png',
+    'assets/images/img4.png',
+    'assets/images/img5.png',
+    'assets/images/img6.png',
+    'assets/images/img7.png',
+  ];
+
+  List<String> _showshop = [
+    'assets/images/shop1.png',
+    'assets/images/shop2.png',
+    'assets/images/shop3.png',
+    'assets/images/shop4.png',
+    'assets/images/shop5.png',
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.04,
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15.0),
+              child: Image.asset(
+                'assets/images/logo.png',
+                width: MediaQuery.of(context).size.width * 0.5,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              'เมนูเด็ดเข็ดฟันน',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.02,
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Container(
+              height: 100.0,
+              padding: EdgeInsets.only(
+                left: 30.0,
+                right: 30.0,
+              ),
+              child: ListView.builder(
+                itemCount: _showMenu.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 5.0,
+                      right: 5.0,
+                    ),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(_showMenu[index])),
+                  );
+                },
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height * 0.03,
+            ),
+            Text(
+              'ร้านเด็ดเจ็ดย่านน้ำ',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.02,
+                color: Color.fromARGB(255, 0, 0, 0),
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: _showshop.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.only(
+                      left: 5.0,
+                      right: 5.0,
+                    ),
+                    child: ClipRRect(
+                        borderRadius: BorderRadius.circular(10),
+                        child: Image.asset(_showshop[index])),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
